690999d75a88a9b962472a7dd67a9d6d4f4705ef
trtec <trtec@DESKTOP-GO9OKHR.fritz.box>
2018-06-17 23:17:13 +0200

    odd or even chaoter 3.2 quiz 2

--- Chapter 3/chapter3_2.cpp
+++ Chapter 3/chapter3_2.cpp
@@ -9,14 +9,29 @@
 
 
 
-
+bool isEven(int number)
+{
+	if (number % 2 == 0)
+		return 1;
+	else
+		return 0;
+}
 
 
 int main()
 {
+	std::cout << "Enter an integer: ";
+	    int x;
+	    std::cin >> x;
+
+	    if (isEven(x))
+	        std::cout << x << " is even\n";
+	    else
+	        std::cout << x << " is odd\n";
+
+	   return 0;
 
 
-	return 0;
 }
 
 
